ff6b83faa42f640eef5e442fa805dabf332e8668
LJH0716 <jhmk0805@naver.com>
2022-04-19 10:32:56 +0900

    2022-04-19 2

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -1,10 +1,25 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex17 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요: ");
+		
+		int num = sc.nextInt();
+		
+		for(int x=num; x>=1;x--) {
+			for(int y=1; y<=x;y++ ) {
+			System.out.print("*");
+			}
+		System.out.println("");	
+		
+		}
+		sc.close();
 	}
-
 }
