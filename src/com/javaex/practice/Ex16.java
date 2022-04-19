f9186b44d782145a17869be0a2c0aeb44ce3baba
LJH0716 <jhmk0805@naver.com>
2022-04-19 10:20:00 +0900

    2022-04-19 1

--- src/com/javaex/practice/Ex16.java
+++ src/com/javaex/practice/Ex16.java
@@ -1,9 +1,40 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int many =0;
+		int sum=0;
+		
+		System.out.print("숫자를 입력하세요: ");
+		
+		int num = sc.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			if (i%5 == 0) {
+				many+=1;
+				sum+=i;
+				
+			}
+		}
+		
+		System.out.println("5의배수의 개수:"+many);
+		System.out.println("5의배수의 합 :"+sum);
+		
+		sc.close();
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
