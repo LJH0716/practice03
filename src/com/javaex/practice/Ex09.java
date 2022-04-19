0a7ba605fd1d478bd0cb333cd1cf6143af742868
LJH0716 <jhmk0805@naver.com>
2022-04-20 00:32:04 +0900

    2022-04-20

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -3,8 +3,14 @@ package com.javaex.practice;
 public class Ex09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for (int x=1; x<= 10; x++) {
+			for (int y=x; y<=10+x; y++) {
+				System.out.print(y + "   ");
+			}
+			System.out.println();
 
+		}
 	}
 
 }
